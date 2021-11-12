71dbc48b89edd15b8fa83f94f0323ce14967fb75
Elizabeth <eli.arriola23@gmail.com>
2021-11-12 13:09:23 -0300

    presentacion con PHP

---
+++ prueba-1/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Presentacion con PHP</h1>
+</body>
+</html>
+
+
+<?php
+
+$nombre= "Ruth Elizabeth";
+$apellido= "Arriola";
+$edad= 32;
+$hobbie= "Handball";
+$editor_de_codigo= "Visual Studio Code";
+$sistema_op= "Linux";
+
+
+echo "Mi nombre es: ".$nombre. " y mi apellido: ".$apellido;
+
+echo '<br>';
+
+echo "Tengo ".$edad." años";
+
+echo '<br>';
+
+echo "Mi hobbie es: ".$hobbie;
+
+echo '<br>';
+
+echo "Mi editor de codigo favorito es: ".$editor_de_codigo. " y el sistema operativo que utilizo es: ".$sistema_op;
+
+
+
+
+
+?>
+
